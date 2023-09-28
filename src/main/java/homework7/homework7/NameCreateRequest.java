86f53cff731011e0d357aa25c0de5f365d535cf1
otyacazyu <vesperia.not.xperia@gmail.com>
2023-09-28 20:45:17 +0900

    現状更新

--- src/main/java/homework7/homework7/NameCreateRequest.java
+++ src/main/java/homework7/homework7/NameCreateRequest.java
@@ -1,14 +1,21 @@
 package homework7.homework7;
 
 public class NameCreateRequest {
-    private String message;
+    private String familyName;
+    private String middleName;
 
-    public NameCreateRequest(String message) {
-        this.message = message;
+    public NameCreateRequest(String familyName, String middleName) {
+        this.familyName = familyName;
+        this.middleName = middleName;
     }
 
+    public String getFamilyName() {
+        return familyName;
+    }
 
-    public String getMessage() {
-        return message;
+    public String getMiddleName() {
+        return middleName;
     }
 }
+
+
